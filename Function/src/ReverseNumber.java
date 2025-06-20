b4f3a735e383938844375265477130e941fce418
Soham Sarkar <srutidhara153@gmail.com>
2025-06-20 22:47:09 +0530

    Function (Reverse)

---
+++ Function/src/ReverseNumber.java
@@ -0,0 +1,17 @@
+public class ReverseNumber {
+    public static void main(String[] args) {
+        int num = 1234636;
+//        int sum =0;
+        int res = reverse(num);
+        System.out.println(res);
+    }
+    static int reverse(int num){
+        int sum =0;
+        while(num>0){
+            int rem = num %10;
+            sum = (sum*10)+rem;
+            num /= 10;
+        }
+        return sum;
+    }
+}
